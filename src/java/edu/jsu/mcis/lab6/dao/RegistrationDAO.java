4df40254d5a097c41fccce7490c3ae2377e5c014
Favour Ejemole <ejemolefavour@gmail.com>
2022-10-13 17:09:05 -0500

    "RegistrationDAO working"

--- src/java/edu/jsu/mcis/lab6/dao/RegistrationDAO.java
+++ src/java/edu/jsu/mcis/lab6/dao/RegistrationDAO.java
@@ -3,6 +3,7 @@ package edu.jsu.mcis.lab6.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import org.json.simple.*;
 
 public class RegistrationDAO {
@@ -13,6 +14,9 @@ public class RegistrationDAO {
             + "((registration JOIN attendee ON registration.attendeeid = attendee.id) "
             + "JOIN `session` ON registration.sessionid = `session`.id) "
             + "WHERE `session`.id = ? AND attendee.id = ?";
+    private final String QUERY_CREATE = "INSERT INTO registration (attendeeid, sessionid)" + "VLAUES (?,?)";
+    private final String QUERY_UPDATE = "UPDATE registration SET attendeeid = ? AND sessionid = ?" + "WHERE (attendeeid = ? AND sessionid = ?)";
+    private final String QUERY_DELETE = "DELETE FROM registration WHERE (attendeeid = ? AND sessionid = ?";
     
     RegistrationDAO(DAOFactory dao) {
         this.daoFactory = dao;
@@ -58,6 +62,173 @@ public class RegistrationDAO {
         catch (Exception e) {
             e.printStackTrace();
         }
+        finally {
+
+            if (rs != null) {
+                try {
+                    rs.close();
+                    rs = null;
+                }
+                catch (Exception e) { e.printStackTrace(); }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                    ps = null;
+                }
+                catch (Exception e) { e.printStackTrace(); }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                    conn = null;
+                }
+                catch (Exception e) { e.printStackTrace(); }
+            }
+
+        }
+
+        return JSONValue.toJSONString(json);
+
+    }
+    
+    //To create registration for a new attendee
+    
+    public String create (int sessionid, int attendeeid) {
+        JSONObject json = new JSONObject();
+        
+        json.put("success", false);
+        
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Connection conn = daoFactory.getConnection();
+        
+        try{
+            
+            ps = conn.prepareStatement(QUERY_CREATE, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, attendeeid);
+            ps.setInt(2, sessionid);
+            
+            int updateCount = ps.executeUpdate();
+            
+            if (updateCount > 0 ) {
+                json.put("success", true);
+                json.put("rowsAffected", updateCount);
+            }
+        }
+        catch(Exception e) {e.printStackTrace();}
+        finally {
+
+            if (rs != null) {
+                try {
+                    rs.close();
+                    rs = null;
+                }
+                catch (Exception e) { e.printStackTrace(); }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                    ps = null;
+                }
+                catch (Exception e) { e.printStackTrace(); }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                    conn = null;
+                }
+                catch (Exception e) { e.printStackTrace(); }
+            }
+
+        }
+
+        return JSONValue.toJSONString(json);
+    }
+    
+    //To update an existing registration
+    public String update(int sessionid_old, int attendeeid_old, int sessionid_updated, int attendeeid_updated) {
+        JSONObject json = new JSONObject();
+        
+        json.put("success", false);
+        
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Connection conn = daoFactory.getConnection();
+        
+        try {
+            
+            ps = conn.prepareStatement(QUERY_UPDATE);
+            ps.setInt(1, attendeeid_old);
+            ps.setInt(2, sessionid_old);
+            ps.setInt(3, attendeeid_updated);
+            ps.setInt(4, sessionid_updated);
+            
+            int updateCount = ps.executeUpdate();
+            
+            if (updateCount > 0) {
+                json.put("success", true);
+                json.put("rowsAffected", updateCount);
+            }
+        }
+        catch(Exception e){e.printStackTrace();}
+        
+        finally {
+
+            if (rs != null) {
+                try {
+                    rs.close();
+                    rs = null;
+                }
+                catch (Exception e) { e.printStackTrace(); }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                    ps = null;
+                }
+                catch (Exception e) { e.printStackTrace(); }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                    conn = null;
+                }
+                catch (Exception e) { e.printStackTrace(); }
+            }
+
+        }
+
+        return JSONValue.toJSONString(json);
+    }
+    
+    //To cancel or delete a registration
+    public String delete (int attendeeid, int sessionid) {
+    
+        JSONObject json = new JSONObject();
+        
+        json.put("success", false);
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Connection conn = daoFactory.getConnection();
+        
+        try {
+        
+            ps = conn.prepareStatement(QUERY_DELETE);
+            
+            ps.setInt(1, attendeeid);
+            ps.setInt(2, sessionid);
+            
+            int updateCount = ps.executeUpdate();
+            
+            if (updateCount > 0 ) {
+            
+                json.put("success", true);
+                json.put("rowsAffected", updateCount);
+            }
+        }
+        catch (Exception e){e.printStackTrace();}
+        
         finally {
 
             if (rs != null) {
